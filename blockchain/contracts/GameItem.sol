caa377943c21ce6a6d9dff66af2e290847677e8b
Taylor Marlow <metallicax4xyou@gmail.com>
2025-04-03 05:47:29 -0400

    Update GameItem.sol

--- blockchain/contracts/GameItem.sol
+++ blockchain/contracts/GameItem.sol
@@ -6,37 +6,32 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract GameItem is ERC721, Ownable {
-        using Counters for Counters.Counter;
-            Counters.Counter private _tokenIdCounter;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
 
-                constructor() ERC721("LifeIsFeudalItem", "LIFI") Ownable(msg.sender) {
-                            // Ownable(msg.sender) sets the deployer as the initial owner
-                }
+    constructor() ERC721("LifeIsFeudalItem", "LIFI") Ownable(msg.sender) {
+        // Ownable(msg.sender) sets the deployer as the initial owner
+    }
 
-                    function safeMint(address to) public onlyOwner {
-                                uint256 tokenId = _tokenIdCounter.current();
-                                        _tokenIdCounter.increment();
-                                                _safeMint(to, tokenId);
-                    }
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
 
-                        // --- Overrides required by Solidity 0.8+ with multiple inheritance ---
-                            function _update(address to, uint256 tokenId, address auth)
-                                    internal
-                                            override(ERC721, Ownable)
-                                                    returns (address)
-                                                        {
-                                                                    return super._update(to, tokenId, auth);
-                                                        }
+    // --- Overrides required by Solidity 0.8+ with multiple inheritance ---
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, Ownable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
 
-                                                            function _increaseBalance(address account, uint128 value)
-                                                                    internal
-                                                                            override(ERC721, Ownable)
-                                                                                {
-                                                                                            super._increaseBalance(account, value);
-                                                                                }
-}
-                                                                                }
-                                                        }
-                    }
-                }
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, Ownable)
+    {
+        super._increaseBalance(account, value);
+    }
 }
